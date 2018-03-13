a4e7b2a56c239c7be8d945ebd4a503095fc30671
lshaci <lshaci@qq.com>
2018-03-13 21:58:10 +0800

    Add session user utils

---
+++ framework-parent/framework-web/src/main/java/top/lshaci/framework/web/utils/SessionUserUtils.java
@@ -0,0 +1,36 @@
+package top.lshaci.framework.web.utils;
+
+import java.util.Objects;
+
+/**
+ * Session User Utils
+ * 
+ * @author lshaci
+ * @since 0.0.4
+ */
+public class SessionUserUtils {
+	
+	/**
+	 * The key of user in session
+	 */
+	private final static String USER_IN_SESSION = "top.lshaci.framework.web.utils.SessionUserUtils.userInSession";
+
+	/**
+	 * Set user in session
+	 * 
+	 * @param user the user
+	 */
+	public static void setUserInSession(Object user) {
+		Objects.requireNonNull(user, "The user must not be null!");
+		HttpRequestUtils.setSessionAttribute(USER_IN_SESSION, user);
+	}
+	
+	/**
+	 * Get user in session
+	 * 
+	 * @return the user in session
+	 */
+	public static Object getUserInSession() {
+		return HttpRequestUtils.getSessionAttribute(USER_IN_SESSION);
+	}
+}
