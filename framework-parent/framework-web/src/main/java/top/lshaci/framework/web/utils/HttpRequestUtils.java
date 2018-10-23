a37e9125642d7e26999a5e00a457d43b7004420c
lshaci <lshaci@qq.com>
2018-10-23 10:35:48 +0800

    <pref>优化getIp

--- framework-parent/framework-web/src/main/java/top/lshaci/framework/web/utils/HttpRequestUtils.java
+++ framework-parent/framework-web/src/main/java/top/lshaci/framework/web/utils/HttpRequestUtils.java
@@ -69,17 +69,23 @@ public class HttpRequestUtils {
 	 * @return ip of the client
 	 */
 	public static String getIp() {
-		HttpServletRequest request = get();
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
+	    HttpServletRequest request = get();
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip =  request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
 		return ip;
 	}
 }
