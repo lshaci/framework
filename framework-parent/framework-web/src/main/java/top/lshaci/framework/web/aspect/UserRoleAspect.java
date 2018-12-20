4e12ef393c2afbd54c393ec9769991f2cc41e096
liangxu <lshaci@qq.com>
2018-12-20 10:54:08 +0800

    <del>删除不用的包

--- framework-parent/framework-web/src/main/java/top/lshaci/framework/web/aspect/UserRoleAspect.java
+++ framework-parent/framework-web/src/main/java/top/lshaci/framework/web/aspect/UserRoleAspect.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import top.lshaci.framework.web.annotation.IgnoreRole;
 import top.lshaci.framework.web.annotation.NeedRole;
 import top.lshaci.framework.web.exception.RolePermissionException;
-import top.lshaci.framework.web.exception.WebBaseException;
 import top.lshaci.framework.web.utils.HttpRequestUtils;
 import top.lshaci.framework.web.utils.SessionUserUtils;
 
