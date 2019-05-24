a9676ddb167637bfc06abb3a535f0504984cf533
liangxu <lshaci@qq.com>
2019-05-24 11:28:51 +0800

    <style>format

--- framework-parent/framework-fastdfs/src/main/java/top/lshaci/framework/fastdfs/util/FastDFSClient.java
+++ framework-parent/framework-fastdfs/src/main/java/top/lshaci/framework/fastdfs/util/FastDFSClient.java
@@ -612,11 +612,9 @@ public class FastDFSClient {
 	 */
 	public static String info() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("{\n")
-		  .append("  The file server address is: ")
+		sb.append("{\n  The file server address is: ")
 		  .append(pool.getFileServerAddr())
-		  .append("\n  ")
-		  .append("The max file size is: ")
+		  .append("\n  The max file size is: ")
 		  .append(pool.getMaxFileSize())
 		  .append("\n}");
 		return sb.toString();
