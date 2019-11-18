7432f3c6a50eca822aadfbeac50b646c8aefa537
liangxu <lshaci@qq.com>
2019-11-18 11:54:50 +0800

    <fix>BaseException添加code

--- framework-parent/framework-common/src/main/java/top/lshaci/framework/common/exception/BaseException.java
+++ framework-parent/framework-common/src/main/java/top/lshaci/framework/common/exception/BaseException.java
@@ -1,20 +1,44 @@
 package top.lshaci.framework.common.exception;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
- * Framework common base exception
- * 
+ * <p>Framework common base exception</p> <br>
+ *
+ * <b>1.0.4: </b>Add field code <br>
  * @author lshaci
  * @since 0.0.1
+ * @version 1.0.4
  */
+@Getter
+@NoArgsConstructor
 public class BaseException extends RuntimeException {
 
 	private static final long serialVersionUID = -8170023045088441807L;
 
-	public BaseException() {
-	}
+	/**
+	 * Base exception code
+	 */
+	private int code;
 
+	/** Constructs a base exception with the specified detail message.
+	 *
+	 * @param message the detail message.
+	 */
 	public BaseException(String message) {
 		super(message);
+	}
+
+	/** Constructs a base exception with the specified detail message and code.
+	 *
+	 * @param code    the exception code.
+	 * @param message the detail message.
+	 * @since 1.0.4
+	 */
+	public BaseException(int code, String message) {
+		super(message);
+		this.code = code;
 	}
 
 	public BaseException(Throwable cause) {
