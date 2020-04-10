06ab63df80e4dcbcca2bd7b48dd2fa760827d3bd
liangxu <lshaci@qq.com>
2020-04-10 17:30:53 +0800

    labelValue

---
+++ framework-common/src/main/java/top/lshaci/framework/common/model/LabelValueVO.java
@@ -0,0 +1,17 @@
+package top.lshaci.framework.common.model;
+
+import lombok.Data;
+
+/**
+ * LabelValueVO
+ *
+ * @author lshaci
+ * @since 1.0.7
+ */
+@Data
+public class LabelValueVO {
+
+    private String label;
+
+    private Object value;
+}
