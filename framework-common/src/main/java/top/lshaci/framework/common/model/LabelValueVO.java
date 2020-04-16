52629a9675e0eda405e44c521a3fd543c6c7708e
liangxu <lshaci@qq.com>
2020-04-16 16:25:09 +0800

    javadoc

--- framework-common/src/main/java/top/lshaci/framework/common/model/LabelValueVO.java
+++ framework-common/src/main/java/top/lshaci/framework/common/model/LabelValueVO.java
@@ -1,6 +1,9 @@
 package top.lshaci.framework.common.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 /**
  * LabelValueVO
@@ -9,9 +12,18 @@ import lombok.Data;
  * @since 1.0.7
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class LabelValueVO {
 
+    /**
+     * 标签
+     */
     private String label;
 
+    /**
+     * 值
+     */
     private Object value;
 }
