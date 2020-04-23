d3d5d0970516b142d4158aab2597f272e1200191
liangxu <lshaci@qq.com>
2020-04-23 11:55:27 +0800

    add： label value of

--- framework-common/src/main/java/top/lshaci/framework/common/model/LabelValueVO.java
+++ framework-common/src/main/java/top/lshaci/framework/common/model/LabelValueVO.java
@@ -26,4 +26,15 @@ public class LabelValueVO {
      * 值
      */
     private Object value;
+
+    /**
+     * 根据label和value创建一个LabelValueVO
+     *
+     * @param label 标签
+     * @param value 值
+     * @return LabelValueVO
+     */
+    public static LabelValueVO of(String label, Object value) {
+        return new LabelValueVO(label, value);
+    }
 }
